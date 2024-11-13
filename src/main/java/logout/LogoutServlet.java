061b65cd904988d790bacad450129f980bf5168b
irene <irene.chen01@sjsu.edu>
2024-11-12 23:04:31 -0800

    format logoutservlet

--- src/main/java/logout/LogoutServlet.java
+++ src/main/java/logout/LogoutServlet.java
@@ -19,7 +19,6 @@ public class LogoutServlet extends HttpServlet {
 		super();
 	}
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
