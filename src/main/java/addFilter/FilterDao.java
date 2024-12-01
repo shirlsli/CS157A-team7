a62313cfa3fd584d14269b531e1394a99b0c2236
irene <irene.chen01@sjsu.edu>
2024-11-30 21:41:13 -0800

    move edit active filters to filterDao

--- src/main/java/addFilter/FilterDao.java
+++ src/main/java/addFilter/FilterDao.java
@@ -140,12 +140,12 @@ public class FilterDao {
 	public String deleteFilter(User user, String filter_id) {
 		loadDriver(dbdriver);
 		Connection con = getConnection();
-		
+
 		String successLog = "";
 		try {
-			
+
 			con.setAutoCommit(false);
-			
+
 			try {
 				// remove from user_filter table
 				String deleteUserFilterSQL = "DELETE FROM myflorabase.user_filter WHERE user_id =? and filter_id=?;";
@@ -153,60 +153,60 @@ public class FilterDao {
 				ps.setInt(1, user.getUserId());
 				ps.setInt(2, Integer.parseInt(filter_id));
 				int user_filter_rows = ps.executeUpdate();
-				
+
 				if (user_filter_rows > 0) {
-					System.out.println("Record(s) found in `user_filter` with user_id= "+ user.getUserId() + " and filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
+					System.out.println("Record(s) found in `user_filter` with user_id= " + user.getUserId()
+							+ " and filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
 				} else {
-					System.out.println("No record found in `user_filter` with user_id= "+ user.getUserId() + " and filter_id=" + Integer.parseInt(filter_id));
+					System.out.println("No record found in `user_filter` with user_id= " + user.getUserId()
+							+ " and filter_id=" + Integer.parseInt(filter_id));
 				}
-				
+
 				// remove from filter
 				String deleteFilterSQL = "DELETE FROM myflorabase.filter WHERE filter_id=?;";
 				PreparedStatement ps2 = con.prepareStatement(deleteFilterSQL);
 				ps2.setInt(1, Integer.parseInt(filter_id));
 				int filter_rows = ps2.executeUpdate();
-				
+
 				if (filter_rows > 0) {
-					System.out.println("Record(s) found in `filter` with filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
+					System.out.println("Record(s) found in `filter` with filter_id=" + Integer.parseInt(filter_id)
+							+ " deleted sucessfully.");
 				} else {
 					System.out.println("No record found in `filter` with filter_id=" + Integer.parseInt(filter_id));
 				}
-				
+
 				// remove from within
 				String deleteWithinSQL = "DELETE FROM myflorabase.within WHERE filter_id=?;";
 				PreparedStatement ps3 = con.prepareStatement(deleteWithinSQL);
 				ps3.setInt(1, Integer.parseInt(filter_id));
 				int within_rows = ps3.executeUpdate();
-				
+
 				if (within_rows > 0) {
-					System.out.println("Record(s) found in `within` with filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
+					System.out.println("Record(s) found in `within` with filter_id=" + Integer.parseInt(filter_id)
+							+ " deleted sucessfully.");
 				} else {
 					System.out.println("No record found in `within` with filter_id=" + Integer.parseInt(filter_id));
 				}
-				
+
 				// ensure all operation successful
 				if (user_filter_rows == 1 && filter_rows == 1 && within_rows > 0) {
-                    // If successful, commit the transaction
-                    con.commit();
-                    successLog = "Delete filter successful.";
-                    
-                } else {
-                    // If any operation fails, rollback the transaction
-                    con.rollback();
-                    successLog = "Delete filter failed. Rolling back changes.";
-                   
-                }
-				
-				return successLog;
-				
-			} catch(SQLException e) {
+					// If successful, commit the transaction
+					con.commit();
+					successLog = "Delete filter successful.";
+
+				} else {
+					// If any operation fails, rollback the transaction
+					con.rollback();
+					successLog = "Delete filter failed. Rolling back changes.";
+
+				}
+
+			} catch (SQLException e) {
 				con.rollback();
 				System.out.println("Error deleting filter: " + e.getMessage());
 				return e.getMessage();
 			}
-			
-			
-			
+
 		} catch (SQLException e) {
 			System.out.println("SQLException caught: " + e.getMessage());
 			e.printStackTrace();
@@ -221,6 +221,74 @@ public class FilterDao {
 				}
 			}
 		}
+		return successLog;
+	}
+
+	public String editActiveFilters(String[] activeFilters, String[] inactiveFilters) {
+		loadDriver(dbdriver);
+		Connection con = getConnection();
+
+		String successLog = "active filters updated";
+
+		try {
+			con.setAutoCommit(false);
+
+			try {
+
+				int updatedRows = 0;
+				String activateFiltersSQL = "UPDATE myflorabase.filter SET active=1 WHERE filter_id = ?;";
+				PreparedStatement ps = con.prepareStatement(activateFiltersSQL);
+				if (activeFilters != null) {
+					for (String value : activeFilters) {
+						ps.setInt(1, Integer.parseInt(value));
+						updatedRows += ps.executeUpdate();
+					}
+				}
+
+				String deactivateFiltersSQL = "UPDATE myflorabase.filter SET active=0 WHERE filter_id = ?;";
+				PreparedStatement ps2 = con.prepareStatement(deactivateFiltersSQL);
+				if (inactiveFilters != null) {
+					for (String value : inactiveFilters) {
+						ps2.setInt(1, Integer.parseInt(value));
+						updatedRows += ps2.executeUpdate();
+					}
+				}
+				
+				
+				// ensure all operation successful
+				if (updatedRows > 0) {
+					// If successful, commit the transaction
+					con.commit();
+					successLog = "Change active filter(s) successful.";
+
+				} else {
+					// If any operation fails, rollback the transaction
+					con.rollback();
+					successLog = "Change active filter(s) failed. Rolling back changes.";
+
+				}
+
+			} catch (SQLException e) {
+				con.rollback();
+				System.out.println("Error changing active filters: " + e.getMessage());
+				successLog = e.getMessage();
+			}
+
+		} catch (SQLException e) {
+			System.out.println("SQLException caught: " + e.getMessage());
+			e.printStackTrace();
+			successLog = e.getMessage();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					System.out.println("Failed to close the connection: " + e.getMessage());
+					successLog = e.getMessage();
+				}
+			}
+		}
+		return successLog;
 	}
 
 }
