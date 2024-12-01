10f95a6d757ae6ddf8edc7a76ea2020bb254fb3f
irene <irene.chen01@sjsu.edu>
2024-11-30 21:28:40 -0800

    move delete filter to filterDao

--- src/main/java/addFilter/DeleteFilterServlet.java
+++ src/main/java/addFilter/DeleteFilterServlet.java
@@ -42,93 +42,12 @@ public class DeleteFilterServlet extends HttpServlet {
 		HttpSession session = request.getSession(false);
 		User user = (User) session.getAttribute("user");
 
-		String databaseUser = "root";
-		String databasePassword = System.getenv("DB_PASSWORD");
-
-		java.sql.Connection con = null;
-
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/myFlorabase?autoReconnect=true&useSSL=false",
-					databaseUser, databasePassword);
-			
-			con.setAutoCommit(false);
-			
-			try {
-				// remove from user_filter table
-				String deleteUserFilterSQL = "DELETE FROM myflorabase.user_filter WHERE user_id =? and filter_id=?;";
-				PreparedStatement ps = con.prepareStatement(deleteUserFilterSQL);
-				ps.setInt(1, user.getUserId());
-				ps.setInt(2, Integer.parseInt(filter_id));
-				int user_filter_rows = ps.executeUpdate();
-				
-				if (user_filter_rows > 0) {
-					System.out.println("Record(s) found in `user_filter` with user_id= "+ user.getUserId() + " and filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
-				} else {
-					System.out.println("No record found in `user_filter` with user_id= "+ user.getUserId() + " and filter_id=" + Integer.parseInt(filter_id));
-				}
-				
-				// remove from filter
-				String deleteFilterSQL = "DELETE FROM myflorabase.filter WHERE filter_id=?;";
-				PreparedStatement ps2 = con.prepareStatement(deleteFilterSQL);
-				ps2.setInt(1, Integer.parseInt(filter_id));
-				int filter_rows = ps2.executeUpdate();
-				
-				if (filter_rows > 0) {
-					System.out.println("Record(s) found in `filter` with filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
-				} else {
-					System.out.println("No record found in `filter` with filter_id=" + Integer.parseInt(filter_id));
-				}
-				
-				// remove from within
-				String deleteWithinSQL = "DELETE FROM myflorabase.within WHERE filter_id=?;";
-				PreparedStatement ps3 = con.prepareStatement(deleteWithinSQL);
-				ps3.setInt(1, Integer.parseInt(filter_id));
-				int within_rows = ps3.executeUpdate();
-				
-				if (within_rows > 0) {
-					System.out.println("Record(s) found in `within` with filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
-				} else {
-					System.out.println("No record found in `within` with filter_id=" + Integer.parseInt(filter_id));
-				}
-				
-				// ensure all operation successful
-				if (user_filter_rows == 1 && filter_rows == 1 && within_rows > 0) {
-                    // If successful, commit the transaction
-                    con.commit();
-                    System.out.println("Delete filter successful.");
-                } else {
-                    // If any operation fails, rollback the transaction
-                    con.rollback();
-                    System.out.println("Delete filter failed. Rolling back changes.");
-                }
-				
-			} catch(SQLException e) {
-				con.rollback();
-				System.out.println("Error deleting filter: " + e.getMessage());
-			}
-			
-			
-			
-		} catch (SQLException e) {
-			System.out.println("SQLException caught: " + e.getMessage());
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			System.out.println("MySQL JDBC Driver not found: " + e.getMessage());
-			e.printStackTrace();
-		} finally {
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					System.out.println("Failed to close the connection: " + e.getMessage());
-				}
-			}
-		}
-
+		FilterDao fDao = new FilterDao();
+		String successLog = fDao.deleteFilter(user, filter_id);
+		
 		response.setContentType("text/plain");
 		response.setCharacterEncoding("UTF-8");
-		response.getWriter().write("Log received and processed successfully.");
+		response.getWriter().write(successLog);
 	}
 
 }
--- src/main/java/addFilter/FilterDao.java
+++ src/main/java/addFilter/FilterDao.java
@@ -38,7 +38,8 @@ public class FilterDao {
 		loadDriver(dbdriver);
 		Connection con = getConnection();
 
-		String s = "SELECT * FROM myflorabase.user_filter uf, myflorabase.filter f WHERE user_id ='" + curUser.getUserId() +  "' AND uf.filter_id = f.filter_id AND filter_name ='"+ filterName+"';";
+		String s = "SELECT * FROM myflorabase.user_filter uf, myflorabase.filter f WHERE user_id ='"
+				+ curUser.getUserId() + "' AND uf.filter_id = f.filter_id AND filter_name ='" + filterName + "';";
 		try {
 			PreparedStatement ps = con.prepareStatement(s);
 			ResultSet resultSet = ps.executeQuery();
@@ -52,11 +53,11 @@ public class FilterDao {
 		}
 		return true;
 	}
-	
+
 	public String addNewFilter(User user, String filterName, String[] selectedValues, String filterColor) {
 		loadDriver(dbdriver);
 		Connection con = getConnection();
-		
+
 		try {
 
 			// checking the values:
@@ -136,5 +137,90 @@ public class FilterDao {
 		return "New filter added successfully!";
 	}
 
+	public String deleteFilter(User user, String filter_id) {
+		loadDriver(dbdriver);
+		Connection con = getConnection();
+		
+		String successLog = "";
+		try {
+			
+			con.setAutoCommit(false);
+			
+			try {
+				// remove from user_filter table
+				String deleteUserFilterSQL = "DELETE FROM myflorabase.user_filter WHERE user_id =? and filter_id=?;";
+				PreparedStatement ps = con.prepareStatement(deleteUserFilterSQL);
+				ps.setInt(1, user.getUserId());
+				ps.setInt(2, Integer.parseInt(filter_id));
+				int user_filter_rows = ps.executeUpdate();
+				
+				if (user_filter_rows > 0) {
+					System.out.println("Record(s) found in `user_filter` with user_id= "+ user.getUserId() + " and filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
+				} else {
+					System.out.println("No record found in `user_filter` with user_id= "+ user.getUserId() + " and filter_id=" + Integer.parseInt(filter_id));
+				}
+				
+				// remove from filter
+				String deleteFilterSQL = "DELETE FROM myflorabase.filter WHERE filter_id=?;";
+				PreparedStatement ps2 = con.prepareStatement(deleteFilterSQL);
+				ps2.setInt(1, Integer.parseInt(filter_id));
+				int filter_rows = ps2.executeUpdate();
+				
+				if (filter_rows > 0) {
+					System.out.println("Record(s) found in `filter` with filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
+				} else {
+					System.out.println("No record found in `filter` with filter_id=" + Integer.parseInt(filter_id));
+				}
+				
+				// remove from within
+				String deleteWithinSQL = "DELETE FROM myflorabase.within WHERE filter_id=?;";
+				PreparedStatement ps3 = con.prepareStatement(deleteWithinSQL);
+				ps3.setInt(1, Integer.parseInt(filter_id));
+				int within_rows = ps3.executeUpdate();
+				
+				if (within_rows > 0) {
+					System.out.println("Record(s) found in `within` with filter_id=" + Integer.parseInt(filter_id) + " deleted sucessfully.");
+				} else {
+					System.out.println("No record found in `within` with filter_id=" + Integer.parseInt(filter_id));
+				}
+				
+				// ensure all operation successful
+				if (user_filter_rows == 1 && filter_rows == 1 && within_rows > 0) {
+                    // If successful, commit the transaction
+                    con.commit();
+                    successLog = "Delete filter successful.";
+                    
+                } else {
+                    // If any operation fails, rollback the transaction
+                    con.rollback();
+                    successLog = "Delete filter failed. Rolling back changes.";
+                   
+                }
+				
+				return successLog;
+				
+			} catch(SQLException e) {
+				con.rollback();
+				System.out.println("Error deleting filter: " + e.getMessage());
+				return e.getMessage();
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			System.out.println("SQLException caught: " + e.getMessage());
+			e.printStackTrace();
+			return e.getMessage();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					System.out.println("Failed to close the connection: " + e.getMessage());
+					return e.getMessage();
+				}
+			}
+		}
+	}
 
 }
